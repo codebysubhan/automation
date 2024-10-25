f4485f1a58d83d3f33e325a8dc12fd02c477df43
codebysubhan <bsdsf22m030@pucit.edu.pk>
2024-10-25 21:00:27 +0500

    automated script test 1

---
+++ auto_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd "C:\Users\user\Desktop\automation"
+
+if [[ -n $(git status --porcelain) ]]; then
+    echo "Changes detected. Enter a commit message: "
+    read commit_message
+
+    git add .
+    git commit -m "$commit_message"
+    git push origin main
+else
+    echo "No changes to commit."
+fi
